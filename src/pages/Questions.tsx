883799d5038390f380647320357d4e616eb94b48
Rodrigo Addor <addor@pdsolucoes.com.br>
2021-08-02 23:44:46 -0300

    feat: improve questions page design

--- src/pages/Questions.tsx
+++ src/pages/Questions.tsx
@@ -1,9 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useMemo } from 'react';
 
 import Box from '@material-ui/core/Box';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 
+import Area from '../components/Area';
 import { useReady } from '../hooks/api';
 
 const Questions: React.FC = () => {
@@ -14,21 +15,38 @@ const Questions: React.FC = () => {
     setReady(true, question);
   };
 
+  const hasQuestion = useMemo(() => question.length > 0, [question.length > 0]);
+
   return (
-    <Box display='flex' flexDirection='column'>
-      <TextField
-        variant='filled'
-        label='Question'
-        value={question}
-        onChange={({ target: { value } }) => {
-          setQuestion(value);
-        }}
-        onKeyPress={({ key }) => {
-          if (key === 'enter') handleSubmitQuestion();
-        }}
-      />
-      <Button onClick={handleSubmitQuestion}>Submit</Button>
-    </Box>
+    <Area
+      title='Questions'
+      content={
+        <Box display='flex' flexDirection='column'>
+          <TextField
+            variant='filled'
+            label='Question'
+            focused={hasQuestion || undefined}
+            value={question}
+            onChange={({ target: { value } }) => {
+              setQuestion(value);
+            }}
+            onKeyPress={({ key }) => {
+              if (key === 'Enter') handleSubmitQuestion();
+            }}
+          />
+          <Button
+            variant={hasQuestion ? 'contained' : 'outlined'}
+            color='primary'
+            disableElevation
+            disabled={!hasQuestion}
+            onClick={handleSubmitQuestion}
+            sx={{ borderTopLeftRadius: 0, borderTopRightRadius: 0 }}
+          >
+            Send Question
+          </Button>
+        </Box>
+      }
+    />
   );
 };
 
